ab193a4a4cadc1006719c84ac3c25017f7c0bb86
martinnc21 <martin.cas.nava@gmail.com>
2026-09-23 18:38:35 -0300

    primer componente HEADER

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header style = {{padding: "1rem", background: "#222", color: "white"}}>
+            <h1>Mi catalogo de Peliculas</h1>
+        </header>
+    );
+}
